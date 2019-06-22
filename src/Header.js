4f509eb3f2c0eaf6caa16e49561c9b80fa94b513
Qbisst <ismaelaperez@outlook.com>
2019-06-22 13:52:52 -0400

    created 1st component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Header(props) {
+   return (
+    <header id="header">
+    <a className="logo" href="index.html">704 Yard Sale</a>
+    </header>
+   );
+}
+
+export default Header;
